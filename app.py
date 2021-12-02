2dbf67ca47f110c7a07d570f6d2a78ddc0ecb3b5
Pao <pzsigua@gmail.com>
2021-12-01 19:25:13 -0800

    Update app.py

--- app.py
+++ app.py
@@ -27,18 +27,35 @@ def submit_charity():
     charity = {
         'name': request.form.get('name'),
         'description': request.form.get('description'),
-        'created_at': datetime.datetime.now(),
-        'number_of_donations': 0,
-        'all_donations': []
+        'created_at': datetime.datetime.now()
     }
+
     charities.insert_one(charity)
     return redirect(url_for('index'))
+
+@app.route('/charity/<charity_id>', methods=['GET'])
+def show_charity(charity_id):
+    '''Show one Charity'''
+    this_charity = charities.find_one({'_id': ObjectId(charity_id)})
+    donations_for_charity = donations.find({'charity': ObjectId(charity_id)})
+    return render_template('show_charity.html',  charity=this_charity, donations=donations_for_charity)
 
 @app.route('/charity/<charity_id>/delete', methods=['POST'])
 def delete_charity(charity_id):
     """Delete one playlist."""
     charities.delete_one({'_id': ObjectId(charity_id)})
     return redirect(url_for('index'))
+
+@app.route('/donation/create', methods=['POST'])
+def create_donation():
+    donation = {
+        'name': request.form.get('name'),
+        'amount': request.form.get('amount'),
+        'charity': ObjectId(request.form.get('charity'))
+    }
+
+    donations.insert_one(donation)
+    return redirect(url_for('show_charity', charity_id=request.form.get('charity')))
 
 if __name__ ==' __main__':
     app.run(debug=True)
